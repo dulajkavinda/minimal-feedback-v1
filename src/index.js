217dc1246deb6b28ce9af4ae3992022b231106ea
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-01-31 14:22:53 +0530

    iniital

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
-
+import CloseIcon from '@material-ui/icons/Close'
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div className={styles.test}>
+      Example Component <CloseIcon />: {text}
+    </div>
+  )
 }
